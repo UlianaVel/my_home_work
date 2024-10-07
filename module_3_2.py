ca8c4e7e6200de2251f9bfab99352a7d81c0ef6f
UlianaVel <redolga53@gmail.com>
2024-10-07 20:32:50 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -11,19 +11,26 @@ def  send_email(message, recipient, sender):
                 if recipient == sender:
                     print('Нельзя отправить письмо самому себе!')
 
+
                 elif sender == 'university.help@gmail.com':
                     print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
                     print(f'Текст письма: {message}')
+
                 else:
                     print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
                     print(f'Текст письма: {message}')
     else:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
+type_sender = int(input("отправить письмо со стандартного адреса: university.help@gmail.com (да - 1, нет - 0): "))
 
+if type_sender == 0:
+    sender_1 = input("Введите нестандартный адрес отправителя:")
+    sender = sender_1
+else:
+    sender = 'university.help@gmail.com'
 
-sender = input("Введите адрес отправителя:")
 recipient = input("Введите адрес получателя:")
 message = input('Введите текст письма:')
-send_email(message, recipient, sender)
 
+send_email(message, recipient, sender)
