6f69805297d0bfc99ef954770e25f209542b06ff
UlianaVel <redolga53@gmail.com>
2024-11-05 17:03:28 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import divide
+from true_math import divide_math
+
+divide(69, 3)
+divide_math(69, 3)
+
+divide(3, 0)
+divide_math(3, 0)
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def divide_math(first, second):
+    if second == 0:
+       print (inf)
+    else:
+       print (first/second)
+
+
