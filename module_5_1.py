6c3849ca449eccd5ece666082a54cac4c0c4c0a1
UlianaVel <redolga53@gmail.com>
2024-11-07 15:57:59 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -1,13 +1,20 @@
 class House:
     def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+           self.name = name
+           self.number_of_floors = number_of_floors
 
-        def go_to(new_floor):
-            if new_floor < 1 or new_floor > self.number_of_floors:
-                print("Такого этажа не существует")
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor < 1 or self.new_floor > self.number_of_floors:
+               print("Такого этажа не существует")
+        else:
+               i = 1
+               while i < int(self.new_floor)+1:
+                     print(i)
+                     i +=1
 
-        go_to(new_floor)
 
-new_floor = int(input("введите номер этажа ",))
-House('ЖК Горский', 18)
+obj = House('ЖК Горский', 18)
+obj.go_to(int(input("введите номер этажа ",)))
+
+
